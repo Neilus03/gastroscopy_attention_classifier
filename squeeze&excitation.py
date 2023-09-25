fd4c799f888e4df4bc733b916cd999dcb89c1182
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-25 09:45:50 +0200

    Update squeeze&excitation.py

--- squeeze&excitation.py
+++ squeeze&excitation.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+   
 class SEBlock(nn.Module):
     def __init__(self, channel, reduction=16):
         super(SEBlock, self).__init__()
