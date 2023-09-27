43977e96b9139557caead320bad801f756f7a9cb
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-27 11:29:41 +0200

    Create dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,40 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import pandas as pd
+import os
+import torch
+
+class CustomDataset(Dataset):
+    def __init__(self, image_folder, label_file, transform=None):
+        self.labels = pd.read_excel(label_file)
+        self.image_folder = image_folder
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        # Image loading
+        img_name = os.path.join(self.image_folder, f"{idx+1}.BMP")  # Adjust the file name according to your numbering scheme
+        image = Image.open(img_name)
+        
+        # Labels
+        individual_labels = torch.tensor(self.labels.iloc[idx, :-1].values, dtype=torch.long)  # All but the last column (INDIVIDUAL DOCTORS)
+        final_label = torch.tensor(self.labels.iloc[idx, -1], dtype=torch.long)  # The last column (FINAL)
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, individual_labels, final_label
+
+# Example usage
+if __name__ == "__main__":
+    from torchvision import transforms
+    
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    
+    dataset = CustomDataset(image_folder="C:\Users\jorge\Desktop\all_images_mean_cropped", label_file="C:\Users\jorge\Documents\GitHub\gastroscopy_attention_classifier\full_filtered.xlsx", transform=transform)
+    print(dataset[0])  # Output will be (image, individual_labels, final_label)
