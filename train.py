68c15d1ff8095708021f0e60aa67fd2c651a2fe1
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-28 13:42:54 +0200

    Update train.py

--- train.py
+++ train.py
@@ -2,10 +2,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import random_split, DataLoader
-from efficientnet import initialize_model  
-from dataloader import EGDDataset  
+#from efficientnet import initialize_model
+from dataloader import EGDDataset
+from simplemodel import initialize_model
 from torchvision import transforms
 from torch.utils.tensorboard import SummaryWriter
+import torchvision
 
 # Initialize TensorBoard SummaryWriter
 writer = SummaryWriter()
@@ -21,8 +23,8 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 dataset = EGDDataset(
-    image_folder="C:/Users/neild/OneDrive/Documentos/CVC/EGD_Barcelona/gastroscopy_attention_classifier/all_images_mean_cropped",
-    label_file="C:/Users/neild/OneDrive/Documentos/CVC/EGD_Barcelona/gastroscopy_attention_classifier/full_filtered.xlsx",
+    image_folder="/content/drive/MyDrive/EGD-Barcelona/merged/all_images_mean_cropped",
+    label_file="/content/drive/MyDrive/EGD-Barcelona/merged/full_filtered.xlsx",
     transform=transform
 )
 
@@ -35,35 +37,54 @@ test_size = total_samples - train_size - val_size
 train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
 
 # Initialize DataLoader
-train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
 
 
 # Function to perform training
 def train_model(model, train_loader, val_loader, num_epochs, criterion, optimizer):
     for epoch in range(num_epochs):
         model.train()  # Set the model to training mode
-        
+
         running_loss = 0.0
         for i, (images, individual_labels, final_label) in enumerate(train_loader):
             # Forward pass
             outputs = model(images)
-            
+
+            # Debugging: Check if images and labels match
+            if epoch == 0 and i < 5:  # Check for the first 5 batches in the first epoch
+                import matplotlib.pyplot as plt
+                import numpy as np
+
+                # Function to unnormalize and plot image
+                def imshow(img):
+                    img = img / 2 + 0.5  # Unnormalize
+                    npimg = img.numpy()
+                    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+                # Display images and labels
+                print(f"Labels: {final_label}")
+                imshow(torchvision.utils.make_grid(images))
+                plt.show()
+
+
             # Compute loss
             loss = criterion(outputs, final_label)
-            
+
             # Backward pass and optimization
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
             running_loss += loss.item()
-            
+
             # Write to TensorBoard
             if i % 10 == 9:  # Log every 10 batches
                 writer.add_scalar('Training Loss', running_loss / 10, epoch * len(train_loader) + i)
                 running_loss = 0.0
+
+        print(f'Epoch [{epoch+1}/{num_epochs}], Training Loss: {running_loss:.4f}')
 
         # Validation loss
         model.eval()  # Set the model to evaluation mode
@@ -75,18 +96,18 @@ def train_model(model, train_loader, val_loader, num_epochs, criterion, optimize
                 outputs = model(images)
                 loss = criterion(outputs, final_label)
                 val_loss += loss.item()
-                
+
                 _, predicted = torch.max(outputs, 1)
                 total += final_label.size(0)
                 correct += (predicted == final_label).sum().item()
-                
+
         val_loss = val_loss / len(val_loader)
         val_acc = 100 * correct / total
-        
+
         # Write validation loss and accuracy to TensorBoard
         writer.add_scalar('Validation Loss', val_loss, epoch)
         writer.add_scalar('Validation Accuracy', val_acc, epoch)
-        
+
         print(f'Epoch [{epoch+1}/{num_epochs}], Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_acc:.2f}%')
 
 # Hyperparameters
