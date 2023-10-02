7941f6afdde5101be17cd3d2a0240d3abf6bfc86
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-10-02 10:27:50 +0200

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -3,6 +3,7 @@ from PIL import Image
 import pandas as pd
 import os
 import torch
+from sklearn.model_selection import KFold
 
 class EGDDataset(Dataset):
     def __init__(self, image_folder, label_file, transform=None):
@@ -17,7 +18,7 @@ class EGDDataset(Dataset):
         # Image loading
         img_name = os.path.join(self.image_folder, f"{idx+1}.BMP")  # Adjust the file name according to your numbering scheme
         image = Image.open(img_name)
-        
+
         # Labels
         individual_labels = torch.tensor(self.labels.iloc[idx, :-1].values, dtype=torch.long)  # All but the last column (INDIVIDUAL DOCTORS)
         final_label = torch.tensor(self.labels.iloc[idx, -1], dtype=torch.long)  # The last column (FINAL)
@@ -30,11 +31,11 @@ class EGDDataset(Dataset):
 # Example usage
 if __name__ == "__main__":
     from torchvision import transforms
-    
+
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    
-    dataset = EGDDataset(image_folder="C:/Users/neild/OneDrive/Documentos/CVC/EGD_Barcelona/gastroscopy_attention_classifier/all_images_mean_cropped", label_file="C:/Users/neild/OneDrive/Documentos/CVC/EGD_Barcelona/gastroscopy_attention_classifier/full_filtered.xlsx", transform=transform)
+
+    dataset = EGDDataset(image_folder="/content/drive/MyDrive/EGD-Barcelona/merged/all_images_mean_cropped", label_file="/content/drive/MyDrive/EGD-Barcelona/merged/full_well.xlsx", transform=transform)
     print(dataset[2])  # Output will be (image, individual_labels, final_label)
