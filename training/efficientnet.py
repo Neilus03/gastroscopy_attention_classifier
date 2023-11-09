6c0f357ce8da196adbe457161d353d963f93d759
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-11-09 11:24:47 +0100

    Update efficientnet.py

--- training/efficientnet.py
+++ training/efficientnet.py
@@ -8,17 +8,23 @@ class CustomEfficientNet(nn.Module):
         # Load pre-trained EfficientNetV2 model
         self.backbone = torch.hub.load('hankyul2/EfficientNetV2-pytorch', 'efficientnet_v2_s', pretrained=True, dropout=0.0, stochastic_depth=0.0)
 
-
+        # Replace the last layer with a new, untrained one
         self.classifier = nn.Sequential(
             nn.Linear(1000, 512),
             nn.ReLU(),
-            nn.BatchNorm1d(512),  # Batch normalization
+            #nn.BatchNorm1d(512),  # Batch normalization
             nn.Dropout(0.5),      # Dropout for regularization
             nn.Linear(512, 256),
             nn.ReLU(),
-            nn.BatchNorm1d(256),  # Batch normalization
+            #nn.BatchNorm1d(256),  # Batch normalization
             nn.Dropout(0.5),      # Dropout for regularization
-            nn.Linear(256, num_classes)
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),    # Dropout for regularization
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.5),    # Dropout for regularization
+            nn.Linear(64, num_classes) # Output layer
         )
 
     def forward(self, x):
